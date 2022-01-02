0703bb51e7ec2fb896ee03bd7eab50786f8918de
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-01-02 12:59:53 -0800

    reverted everything back to basic app.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,9 @@
 import "./app.css";
 
 function App() {
-  let title = "hello world title";
-  let paragraph = "whats up paragraph";
-  let comments = [
-    { id: 1, comment: "hello world 1" },
-    { id: 2, comment: "hello world 2" },
-    { id: 3, comment: "hello world 3" },
-  ];
-
-  let loading = false;
-  let showComments = true;
-
   return (
     <div className="app-div">
-      <h1>{title.toLowerCase()}</h1>
-      <p>{paragraph}</p>
-      <div className="map-elements">
-        <h3>comments length ({comments.length})</h3>
-        {showComments ? "comment is True" : "comment is False"}
-        <ul>
-          {comments.map((comment, index) => (
-            <li key={comment.id}>
-              {comment.comment} the index is {index}
-            </li>
-          ))}
-        </ul>
-      </div>
+      <h1>My app</h1>
     </div>
   );
 }
