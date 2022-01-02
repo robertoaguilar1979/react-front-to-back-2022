3507246559a842f824ecc3a44ac90aceff545bcd
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-01-01 22:34:02 -0800

    conditional react

--- src/App.js
+++ src/App.js
@@ -8,12 +8,17 @@ function App() {
     { id: 2, comment: "hello world 2" },
     { id: 3, comment: "hello world 3" },
   ];
+
+  let loading = false;
+  let showComments = true;
+
   return (
     <div className="app-div">
       <h1>{title.toLowerCase()}</h1>
       <p>{paragraph}</p>
       <div className="map-elements">
         <h3>comments length ({comments.length})</h3>
+        {showComments ? "comment is True" : "comment is False"}
         <ul>
           {comments.map((comment, index) => (
             <li key={comment.id}>
