967c7a24018951a6aea9c959304ce1b06af02e5e
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-01-08 22:54:03 -0800

    added useState to FeedBackItem.jsx

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Header from "./components/Header";
 import FeedbackItem from "./components/FeedbackItem";
 function App() {
   return (
-    <div className="app-div">
-      <h1>My app</h1>
+    <>
       <Header text="hello" name="robert" />
-      <FeedbackItem />
-    </div>
+
+      <div className="container">
+        <FeedbackItem />
+      </div>
+    </>
   );
 }
 
