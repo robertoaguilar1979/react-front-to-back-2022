936d57334d7398f1d0aa41cd2f90854c72ba48d6
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-01-13 16:51:12 -0800

    added how many reviews and reviews average rating to component feedbackStats

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,9 +1,25 @@
-function FeedbackStats() {
+import PropTypes from "prop-types";
+
+function FeedbackStats({ feedback }) {
+  //calculate rating average
+
+  let average =
+    feedback.reduce((sum, rating) => {
+      return (sum += rating.rating);
+    }, 0) / feedback.length;
+
+  average = average.toFixed(1).replace(/[.,]0$/, "");
+
   return (
     <div className="feedback-stats">
-      <h4></h4>
+      <h4>{feedback.length} reviews</h4>
+      <h4>Average Rating: {isNaN(average) ? 0 : average}</h4>
     </div>
   );
 }
+
+FeedbackStats.propTypes = {
+  feedback: PropTypes.array.isRequired,
+};
 
 export default FeedbackStats;
