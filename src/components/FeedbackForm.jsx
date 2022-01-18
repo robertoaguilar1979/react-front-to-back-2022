23e7cfe663b7bb2ab107261375bfc21dbdaa6eb6
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-01-18 15:07:09 -0800

    added useState to the input in the form

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,7 +1,28 @@
 import Card from "./shared/Card";
+import { useState } from "react";
 
 function FeedbackForm() {
-  return <Card></Card>;
+  const [text, setText] = useState("");
+
+  const handleTextChange = (e) => {
+    setText(e.target.value);
+  };
+  return (
+    <Card>
+      <form action="">
+        <h2>How would you rate our service with us?</h2>
+        <div className="input-group">
+          <input
+            type="text"
+            placeholder="write a review"
+            onChange={handleTextChange}
+            value={text}
+          />
+          <button type="submit">Send</button>
+        </div>
+      </form>
+    </Card>
+  );
 }
 
 export default FeedbackForm;
